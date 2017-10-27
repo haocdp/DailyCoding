2601a7c621e4a686b653796f2594c6055d25fa39
haocdp <haoc_dp@163.com>
2017-10-27 15:04:01 +0800

    算法题

--- src/main/java/com/haocdp/coding/Main.java
+++ src/main/java/com/haocdp/coding/Main.java
@@ -8,10 +8,27 @@ import com.haocdp.coding.csvCoding.CSVFormatter;
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        String inputFile1 = "/Users/haoc_dp/Desktop/tripsod20140227_1.mpos.csv";
-        String inputFile2 = "/Users/haoc_dp/Desktop/tripsod20140227_2.mpos.csv";
-        String outputFile = "/Users/haoc_dp/Desktop/od.csv";
+//        String inputFile1 = "/Users/haoc_dp/Desktop/tripsod20140227_1.mpos.csv";
+//        String inputFile2 = "/Users/haoc_dp/Desktop/tripsod20140227_2.mpos.csv";
+//        String outputFile = "/Users/haoc_dp/Desktop/od.csv";
+//
+//        CSVFormatter.merge(inputFile1,inputFile2, outputFile);
 
-        CSVFormatter.merge(inputFile1,inputFile2, outputFile);
+        System.out.println(gcd(45, 63));
+    }
+
+
+    /**
+     * 求最大公约数
+     * @param p
+     * @param q
+     * @return
+     */
+    public static int gcd(int p, int q) {
+        if (q == 0)
+            return p;
+
+        int r = p % q;
+        return gcd(q, r);
     }
 }
